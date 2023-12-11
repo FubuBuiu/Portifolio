bb0190bc29db9a9bd23e85cbd7afe5a0dd3fcf41
Brendon Angelo <brendonangelo@hotmail.com>
2023-12-11 15:35:24 -0300

    chore: Styled Components icon added

---
+++ vue-portfolio/svg/StyledComponentsIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <span
+    :style="{
+      fontSize: `${fontSize}px`,
+      lineHeight: 1,
+      color: isLightIcon ? 'white' : 'black',
+    }"
+    >{{ "<💅>" }}</span
+  >
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    fontSize: {
+      type: String,
+      default: undefined,
+    },
+    isLightIcon: { type: Boolean, default: false },
+  },
+});
+</script>
