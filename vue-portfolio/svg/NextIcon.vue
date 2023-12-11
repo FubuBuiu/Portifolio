07afc2b3f145d39153b78c7c9713262cf4c14a28
Brendon Angelo <brendonangelo@hotmail.com>
2023-12-11 15:34:13 -0300

    fix: Icon Next fixed

--- vue-portfolio/svg/NextIcon.vue
+++ vue-portfolio/svg/NextIcon.vue
@@ -1,49 +1,48 @@
 <template>
-   <svg
-      xmlns="http://www.w3.org/2000/svg"
-      :width='width'
-      :height='height'
-      viewBox="0 0 48 48"
-    >
-      <g>
-        <path
-          d="M24 48C37.2548 48 48 37.2548 48 24C48 10.7452 37.2548 0 24 0C10.7452 0 0 10.7452 0 24C0 37.2548 10.7452 48 24 48Z"
-          fill="black"
-        />
-        <path
-          d="M39.8692 42.0052L18.4383 14.3999H14.4004V33.5919H17.6307V18.5022L37.3335 43.9586C38.2225 43.3636 39.0695 42.7106 39.8692 42.0052Z"
-          fill="url(#paint0_linear_558_810)"
-        />
-        <path
-          d="M33.867 14.3999H30.667V33.5999H33.867V14.3999Z"
-          fill="url(#paint1_linear_558_810)"
-        />
-      </g>
-      <defs>
-        <linearGradient
-          id="paint0_linear_558_810"
-          x1="29.0671"
-          y1="31.0666"
-          x2="38.5337"
-          y2="42.7999"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="white" />
-          <stop offset="1" stopColor="white" stopOpacity="0" />
-        </linearGradient>
-        <linearGradient
-          id="paint1_linear_558_810"
-          x1="32.267"
-          y1="14.3999"
-          x2="32.2134"
-          y2="28.4999"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="white" />
-          <stop offset="1" stopColor="white" stopOpacity="0" />
-        </linearGradient>
-      </defs>
-    </svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 256 256"
+    fill="none"
+  >
+    <path
+      d="M128 256C198.692 256 256 198.692 256 128C256 57.3075 198.692 0 128 0C57.3075 0 0 57.3075 0 128C0 198.692 57.3075 256 128 256Z"
+      fill="black"
+    />
+    <path
+      d="M212.63 224.03L98.3398 76.8H76.7998V179.16H94.0298V98.68L199.11 234.45C203.85 231.27 208.37 227.79 212.63 224.03Z"
+      fill="url(#paint0_linear_940_419)"
+    />
+    <path
+      d="M180.63 76.8H163.56V179.2H180.63V76.8Z"
+      fill="url(#paint1_linear_940_419)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_940_419"
+        x1="132.707"
+        y1="181.561"
+        x2="178.076"
+        y2="237.79"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="white" />
+        <stop offset="1" stop-color="white" stop-opacity="0" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_940_419"
+        x1="172.082"
+        y1="76.7712"
+        x2="171.775"
+        y2="151.973"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="white" />
+        <stop offset="1" stop-color="white" stop-opacity="0" />
+      </linearGradient>
+    </defs>
+  </svg>
 </template>
 <script lang="ts">
 import Vue from "vue";
