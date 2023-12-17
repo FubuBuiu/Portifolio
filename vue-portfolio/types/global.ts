5efbf7d4c2f4d84a1867d9f34b8f09e2ecdf5a1b
Brendon Angelo <brendonangelo@hotmail.com>
2023-12-17 01:36:22 -0300

    chore: Global interfaces type file created

---
+++ vue-portfolio/types/global.ts
@@ -0,0 +1,12 @@
+export interface SectionsRefType {
+  startSection: HTMLDivElement;
+  aboutMeSection: HTMLDivElement;
+  skillsSection: HTMLDivElement;
+  projectsSection: HTMLDivElement;
+  contactsSection: HTMLDivElement;
+}
+
+export interface ButtonOptionsInterface {
+  text: string;
+  handleClick: () => void;
+}
